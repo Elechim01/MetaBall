2929a862fa45768fc5e6c0eeed71a7abe6145f27
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-09-24 15:37:34 +0200

    Initial Commit

---
+++ MetaBall/MetaBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaBallApp.swift
+//  MetaBall
+//
+//  Created by Michele Manniello on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MetaBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
